a0984261671579a89d017b746cb98aa2faa8b0d7
fmart3 <felipe.martinez3@mail.udp.cl>
2026-01-25 19:38:53 -0300

    Primer Despliegue

--- App.py
+++ App.py
@@ -104,21 +104,21 @@ from pages.appAlt import page_app_alt
 # =========================
 
 PAGES = {
-#     0: page_app_alt,
-#     1: page_big5_extraversion,
-#     2: page_big5_amabilidad,
-#     3: page_big5_responsabilidad,
-#     4: page_big5_neuroticismo,
-#     5: page_big5_apertura,
-#     6: page_phish_actitud_riesgo,
-#     7: page_phish_awareness,
-#     8: page_phish_riesgo_percibido,
-#     9: page_phish_autoeficacia,
-#    10: page_phish_susceptibilidad,
-#    11: page_fatiga_emocional,
-#    12: page_fatiga_cinismo,
-#    13: page_fatiga_abandono,
-   1: page_demographics,
+    0: page_app_alt,
+    1: page_big5_extraversion,
+    2: page_big5_amabilidad,
+    3: page_big5_responsabilidad,
+    4: page_big5_neuroticismo,
+    5: page_big5_apertura,
+    6: page_phish_actitud_riesgo,
+    7: page_phish_awareness,
+    8: page_phish_riesgo_percibido,
+    9: page_phish_autoeficacia,
+   10: page_phish_susceptibilidad,
+   11: page_fatiga_emocional,
+   12: page_fatiga_cinismo,
+   13: page_fatiga_abandono,
+   14: page_demographics,
    99: page_results
 }
 
